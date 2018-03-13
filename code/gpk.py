ea92f846efa282e23bd5dd644a4247509646fca2
yangkky <seinchin@gmail.com>
2018-03-13 11:18:10 -0700

    Code and outputs.

---
+++ code/gpk.py
@@ -0,0 +1,288 @@
+"""Kernel functions that calculate the covariance between two inputs."""
+
+import numpy as np
+import pandas as pd
+from sys import exit
+import abc
+
+from scipy.spatial import distance
+
+
+class BaseKernel(abc.ABC):
+
+    """ A Gaussian Process kernel.
+
+       Attributes:
+           hypers (list)
+    """
+
+    @abc.abstractmethod
+    def __init__(self):
+        """ Create a GPKernel. """
+        self._n_hypers = 0
+
+    @abc.abstractmethod
+    def cov(self, X1, X2, hypers=None):
+        """ Calculate the covariance. """
+        return np.zeros((len(X1), len(X2)))
+
+    @abc.abstractmethod
+    def fit(self, X):
+        return self._n_hypers
+
+
+class PolynomialKernel(BaseKernel):
+
+    """ A Polynomial kernel of the form (s0 + sp * x.T*x)^d
+
+    Attributes:
+    hypers (list): names of the hyperparameters required
+    _deg (integer): degree of polynomial
+    """
+
+    def __init__(self, d):
+        """ Initiate a polynomial kernel.
+
+        Parameters:
+            d (integer): degree of the polynomial
+        """
+        if not isinstance(d, int):
+            raise TypeError('d must be an integer.')
+        if d < 1:
+            raise ValueError('d must be greater than or equal to 1.')
+        self._n_hypers = 2
+        self._deg = d
+        self._saved = None
+        self._X = None
+
+    def fit(self, X):
+        """ Remember an input. """
+        self._saved = X @ X.T
+        return self._n_hypers
+
+    def cov(self, X1=None, X2=None, hypers=(1.0, 1.0)):
+        """ Calculate the polynomial covariance matrix between X1 and X2.
+
+        If neither X1 nor X2 is given, they are assumed to be saved from fit.
+
+        Parameters:
+            X1 (np.ndarray):
+            X2 (np.ndarray):
+            hypers (iterable):
+
+        Returns:
+            K (np.ndarray)
+        """
+        sigma_0, sigma_p = hypers
+        if X1 is None and X2 is None:
+            return np.power(sigma_0 ** 2 + sigma_p ** 2 * self._saved,
+                            self._deg)
+        return np.power(sigma_0 ** 2 + sigma_p ** 2 * X1 @ X2.T, self._deg)
+
+
+class BaseRadialKernel(BaseKernel):
+
+    """ Base class for radial kernel functions. """
+
+    def __init__(self):
+        self._n_hypers = 1
+        return
+
+    def cov(self, X1, X2, hypers=None):
+        return BaseKernel.cov(X1, X2)
+
+    def fit(self, X):
+        self._saved = self._distance(X, X)
+        return self._n_hypers
+
+    def _distance(self, X1, X2):
+        """ Calculates the squared distances between rows of X1 and X2.
+
+        Each row of X1 and X2 represents one measurement. Each column
+        represents a dimension.
+
+        Parameters:
+            X1 (np.ndarray): n x d
+            X2 (np.ndarray): m x d
+
+        Returns:
+            D (np.ndarray): n x m
+        """
+        return distance.cdist(X1, X2, metric='sqeuclidean')
+
+
+class MaternKernel(BaseRadialKernel):
+
+    """ A Matern kernel with nu = 5/2 or 3/2.
+
+    Attributes:
+        hypers (list): names of the hyperparameters required
+        _saved_X (dict): dict of saved index:X pairs
+        nu (string): '3/2' or '5/2'
+    """
+
+    def __init__(self, nu):
+        """ Initiate a Matern kernel.
+
+        Parameters:
+            nu (string): '3/2' or '5/2'
+        """
+        if nu not in ['3/2', '5/2']:
+            raise ValueError("nu must be '3/2' or '5/2'")
+        self._n_hypers = 1
+        self.nu = nu
+
+    def fit(self, X):
+        BaseRadialKernel.fit(self, X)
+        self._saved = np.sqrt(self._saved)
+        return self._n_hypers
+
+    def cov(self, X1=None, X2=None, hypers=(1.0, )):
+        """ Calculate the Matern kernel between X1 and X2.
+
+        Parameters:
+            X1 (np.ndarray):
+            X2 (np.ndarray)
+            hypers (iterable): default is ell=1.0.
+
+        Returns:
+            K (np.ndarray)
+        """
+        if X1 is None and X2 is None:
+            D = self._saved
+        else:
+            D = np.sqrt(self._distance(X1, X2))
+        L = hypers[0]
+        D_L = D / L
+        if self.nu == '3/2':
+            return self._m32(D_L)
+        elif self.nu == '5/2':
+            return self._m52(D_L)
+
+    def _m32(self, D_L):
+        return (1.0 + np.sqrt(3.0) * D_L) * np.exp(-np.sqrt(3) * D_L)
+
+    def _m52(self, D_L):
+        first = (1.0 + np.sqrt(5.0)*D_L) + 5.0*np.power(D_L, 2)/3.0
+        second = np.exp(-np.sqrt(5.0) * D_L)
+        return first * second
+
+
+class SEKernel(BaseRadialKernel):
+
+    """ A squared exponential kernel.
+
+    Attributes:
+        hypers (list)
+        _d_squared (np.ndarray)
+        _saved_X (dict)
+    """
+
+    def __init__(self):
+        """ Initiate a SEKernel. """
+        self._n_hypers = 2
+
+    def cov(self, X1=None, X2=None, hypers=(1.0, 1.0)):
+        """ Calculate the squared exponential kernel between x1 and x2.
+
+        Parameters:
+            X1 (np.ndarray):
+            X2 (np.ndarray)
+            hypers (iterable): default is (1.0, 1.0)
+
+        Returns:
+            K (np.ndarray)
+        """
+        sigma_f, ell = hypers
+        if X1 is None and X2 is None:
+            D = self._saved
+        else:
+            D = self._distance(X1, X2)
+        sigma_f, L = hypers
+        D_L2 = D / L ** 2
+        return self._se(D_L2, sigma_f)
+
+    def _se(self, D_L2, sigma_f):
+        return sigma_f ** 2 * np.exp(-0.5 * D_L2)
+
+
+class SumKernel(BaseKernel):
+
+    """
+    A kernel that sums over other kernels.
+
+    Attributes:
+        _kernels (list): list of member kernels
+        hypers (list): the names of the hyperparameters
+    """
+
+    def __init__(self, kernels):
+        '''
+        Initiate a SumKernel containing a list of other kernels.
+
+        Parameters:
+            kernels(list): list of member kernels
+            hypers (list): list of hyperparameter names
+        '''
+        self._kernels = kernels
+
+    def fit(self, X):
+        n_hypers = 0
+        for kernel in self._kernels:
+            n_hypers += kernel.fit(X)
+        return n_hypers
+
+    def cov(self, X1=None, X2=None, hypers=None):
+        """ Calculate the sum kernel between two inputs.
+
+        Parameters:
+            X1 (np.ndarray):
+            X2
+            hypers (iterable): the hyperparameters. Default is to use
+                the defaults for each kernel.
+
+        Returns:
+            K (np.ndarray)
+        """
+        if hypers is None:
+            Ks = [kern.cov(X1, X2) for kern in self._kernels]
+        else:
+            hypers_inds = [k._n_hypers for k in self._kernels]
+            hypers_inds = np.cumsum(np.array(hypers_inds))
+            hypers_inds = np.insert(hypers_inds, 0, 0)
+            Ks = [kern.cov(X1, X2, hypers[hypers_inds[i]:
+                                          hypers_inds[i+1]])
+                  for i, kern in enumerate(self._kernels)]
+        return sum(Ks)
+
+
+class LinearKernel(BaseKernel):
+
+    """ The linear (dot product) kernel for two inputs. """
+
+    def __init__(self):
+        """ Initiates a LinearKernel. """
+        self._n_hypers = 1
+
+    def fit(self, X):
+        self._saved = X @ X.T
+        return self._n_hypers
+
+    def cov(self, X1=None, X2=None, hypers=(1.0, )):
+        """ Calculate the linear kernel between x1 and x2.
+
+        The linear kernel is the dot product of x1 and x2 multiplied
+        by a scaling variable var_p.
+
+        Parameters:
+            x1 (np.ndarray)
+            x2 (np.ndarray)
+            hypers (iterable): default is var_p=1.0.
+
+        Returns:
+            K (np.ndarray)
+        """
+        vp = hypers[0]
+        if X1 is None and X2 is None:
+            return vp * self._saved
+        return vp * X1 @ X2.T
---
+++ code/stringkernel.py
@@ -0,0 +1,143 @@
+import itertools
+from collections import Counter
+from gpk import BaseKernel
+import numpy as np
+
+class MismatchKernel(BaseKernel):
+
+    """
+    A mismatch kmer kernel as described by Leslie et al. here:
+    https://academic.oup.com/bioinformatics/article/20/4/467/192308
+
+    Attributes:
+        k (int): size of the kmers
+        A (list): allowed alphabet. Elements must be strings.
+        m (int): allowed mismatches
+    """
+
+    def __init__(self, k, A, m):
+        """ Instantiate a MismatchKernel.
+
+        Parameters:
+            k (int): size of the kmers
+            A (list): allowed alphabet. Elements must be strings.
+            m (int): allowed mismatches
+        """
+        self.k = k
+        self.A = A
+        self.m = m
+        self.A_to_num = {a:i for i, a in enumerate(A)}
+        self.num_to_A = {i:a for i, a in enumerate(A)}
+        self.nums = list(range(len(A)))
+        self.nodes = self.make_kmer_tree(k, self.nums)
+        self._n_hypers = 1
+        self._saved = None
+        return
+
+    def make_kmer_tree(self, k, nums):
+        """ Return a list representing the kmer tree."""
+        nodes = [(np.array([]), [])]
+        for it in range(k):
+            new_nodes = []
+            count = 0
+            for i, node in enumerate(nodes):
+                n, e = node
+                if len(n) < it:
+                    continue
+                for a in nums:
+                    count += 1
+                    new_node = (np.append(n, a), [])
+                    new_nodes.append(new_node)
+                    nodes[i][1].append(len(nodes) + count - 1)
+            nodes += new_nodes
+        return nodes
+
+    def prune(self, candidates, mutations, prefix):
+        """
+        Candidates are indices to kmer candidates,
+        mutations are corresponding mutation counts
+        prefix is kmer as vector
+        """
+        L = len(prefix)
+        if L == 0:
+            return candidates, mutations
+        mutant = self.observed[candidates][:, L - 1] != prefix[-1]
+        mutations[candidates] += mutant
+        keep_me = mutations <= self.m
+        candidates = candidates[keep_me[candidates]]
+        return candidates, mutations
+
+    def fit(self, seqs):
+        """ Precompute the kernel for a set of sequences."""
+        self._saved = self.cov(seqs1=seqs, seqs2=seqs)
+        return self._n_hypers
+
+    def cov(self, seqs1=None, seqs2=None, hypers=(1.0,)):
+        """Calculate the mismatch string kernel.
+
+        If no sequences given, then uses precomputed kernel.
+
+        Parameters:
+            seqs1 (list): list of strings
+            seqs2 (list): list of strings
+            hypers (iterable): the sigma value
+
+        Returns:
+            K (np.ndarray): n1 x n2 normalized mismatch string kernel
+        """
+        if seqs1 is None and seqs2 is None:
+            return self._saved * hypers[0]
+        if not isinstance(seqs1, list):
+            seqs1 = list(seqs1)
+        if not isinstance(seqs2, list):
+            seqs2 = list(seqs2)
+        # Break seqs into kmers
+        kmers1 = [[seq[i:i + self.k] for i in range(len(seq) - self.k + 1)] for seq in seqs1]
+        kmers2 = [[seq[i:i + self.k] for i in range(len(seq) - self.k + 1)] for seq in seqs2]
+        # Get all observed kmers
+        self.observed = sorted(set(itertools.chain.from_iterable(kmers1 + kmers2)))
+        # Get count of each observed kmer for each sequence
+        self.X1 = np.zeros((len(seqs1), len(self.observed)))
+        self.X2 = np.zeros((len(seqs2), len(self.observed)))
+        kmer_counts1 = [Counter(kmer) for kmer in kmers1]
+        kmer_counts2 = [Counter(kmer) for kmer in kmers2]
+        for j, obs in enumerate(self.observed):
+            for i, counts in enumerate(kmer_counts1):
+                self.X1[i, j] = counts[obs]
+            for i, counts in enumerate(kmer_counts2):
+                self.X2[i, j] = counts[obs]
+        # Convert observed kmers to an array
+        self.observed = np.array([[self.A_to_num[a] for a in obs] for obs in self.observed])
+        # Create the covariance matrix
+        self.K = np.zeros((len(seqs1), len(seqs2)))
+        # Create the variance matrices
+        self.K11 = np.zeros((len(seqs1), 1))
+        self.K22 = np.zeros((len(seqs2), 1))
+        # Initialize the mutation counts
+        mutations = np.zeros(len(self.observed))
+        # Initialize the candidate indices
+        candidates = np.arange(len(mutations))
+        # Populate K
+        self.dft(candidates.copy(), mutations.copy(), 0)
+        # Normalize K
+        self.K /= np.sqrt(self.K11)
+        self.K /= np.sqrt(self.K22.T)
+        self.K *= hypers[0]
+        return self.K
+
+    def dft(self, candidates, mutations, ind):
+        """ Depth first traversal of kmer tree to calculate K."""
+        kmer = self.nodes[ind][0]
+        candidates, mutations = self.prune(candidates, mutations, kmer)
+        if len(candidates) == 0:
+            return
+        if len(self.nodes[ind][1]) == 0:
+            Y = np.zeros((len(self.observed), 1))
+            Y[candidates, 0] = 1
+            n_alphas1 = self.X1 @ Y
+            n_alphas2 = self.X2 @ Y
+            self.K += n_alphas1 @ n_alphas2.T
+            self.K11 += n_alphas1 ** 2
+            self.K22 += n_alphas2 ** 2
+        for e in self.nodes[ind][1]:
+            self.dft(candidates.copy(), mutations.copy(), e)
